45375dcded1d618a9439b1de34d6ca03a3e2f1a4
IA7308 <naziqnaz13@gmail.com>
2024-04-19 20:12:41 +0700

    payments ATM method

--- mankof_apps/lib/main.dart
+++ mankof_apps/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'releasev1.0.0',
+      title: 'Payments ATM',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
