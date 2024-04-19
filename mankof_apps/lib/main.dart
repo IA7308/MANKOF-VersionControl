d2c5449163f29512b3fb1520ceb9d574e4a947ff
IA7308 <naziqnaz13@gmail.com>
2024-04-19 20:03:18 +0700

    implement Backend

--- mankof_apps/lib/main.dart
+++ mankof_apps/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'status-pesanan',
+      title: 'status-pesanan Feature',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
